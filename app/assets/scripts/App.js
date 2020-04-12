9c5bb565cf62c358c51f2b68dfd939febf4d54fe
David G <davidgoubau@gmail.com>
2020-04-12 11:14:53 +0800

    Completed reveal animations

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.css";
-import MobileMenu from "./modules/MobileMenu.js";
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(".feature-item", 75);
+new RevealOnScroll(".testimonial", 60);
 
 if (module.hot) {
     module.hot.accept();

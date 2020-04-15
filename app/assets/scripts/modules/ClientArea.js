df327a11589c4832583aabefc9edc8b41092c855
David G <davidgoubau@gmail.com>
2020-04-15 18:02:07 +0800

    update Client area

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -1,24 +1,24 @@
 import Axios from "axios";
+
 class ClientArea {
     constructor() {
         this.injectHTML();
         this.form = document.querySelector(".client-area__form");
         this.field = document.querySelector(".client-area__input");
         this.contentArea = document.querySelector(".client-area__content-area");
-
         this.events();
     }
 
     events() {
-        this.form.addEventListener("submit", (event) => {
-            event.preventDefault();
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
             this.sendRequest();
         });
     }
 
     sendRequest() {
         Axios.post(
-            "https://boring-ritchie-126b8a.netlify.com/.netlify/functions/secret-area",
+            "https://pensive-rosalind-383c21.netlify.com/.netlify/functions/secret-area",
             { password: this.field.value }
         )
             .then((response) => {
@@ -26,8 +26,7 @@ class ClientArea {
                 this.contentArea.innerHTML = response.data;
             })
             .catch(() => {
-                this.contentArea.innerHTML(`
-            <p class="client-area__error>That secret phrase is not correct</p>`);
+                this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again.</p>`;
                 this.field.value = "";
                 this.field.focus();
             });
@@ -37,17 +36,17 @@ class ClientArea {
         document.body.insertAdjacentHTML(
             "beforeend",
             `
-            <div class="client-area">
-                <div class="wrapper wrapper--medium">
-                    <h2 class="section-title section-title--blue">Secret Client Area</h2>
-                    <form class="client-area__form" action="">
-                    <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
-                    <button class="btn btn--orange">Submit</button>
-                    </form>
-                    <div class="client-area__content-area"></div>
-                </div>
-            </div>
-            `
+    <div class="client-area">
+      <div class="wrapper wrapper--medium">
+        <h2 class="section-title section-title--blue">Secret Client Area</h2>
+        <form class="client-area__form" action="">
+          <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+          <button class="btn btn--orange">Submit</button>
+        </form>
+        <div class="client-area__content-area"></div>
+      </div>
+    </div>
+    `
         );
     }
 }
